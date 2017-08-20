d6601cbe908b271ebda099afb96e1dd102e51f9f
Junjie <junjie@gbox.adnu.edu.ph>
2017-08-20 20:16:35 +0800

    added constraint for person.prsn_lname and prsn_fname to 'not null'.

--- doc/db structure/create tables.sql
+++ doc/db structure/create tables.sql
@@ -1,8 +1,8 @@
 create table person (
     prsn_id number(2),
-    prsn_lname varchar(30),
+    prsn_lname varchar(30) not null,
     prsn_lnamesuffix varchar(10),
-    prsn_fname varchar(30),
+    prsn_fname varchar(30) not null,
     prsn_fnamesuffix varchar(10),
     prsn_mname varchar(30),
     prsn_minitial varchar(10)
