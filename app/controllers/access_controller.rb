64120a78232d9f289c783241289ebf980fe8a200
Junjie <junjie@gbox.adnu.edu.ph>
2017-08-23 21:22:03 +0800

    added access/LadgerHash.

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,4 +1,15 @@
 class AccessController < ApplicationController
-  def login
-  end
+	def login
+
+	end
+
+	private
+	class LadgerHash
+		require 'digest';
+		def hash(raw)
+			sha256 = Digest::SHA256.new;
+			salt = sha256.hexdigest(raw);
+			return sha256.hexdigest(raw+salt)[0..39];
+		end
+	end
 end
