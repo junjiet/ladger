3cf4bf2d242b6033e560a15a6f94501e3f9929fe
Junjie <junjie@gbox.adnu.edu.ph>
2017-08-28 17:32:22 +0800

    added view_person.

---
+++ doc/db structure/view view_person.sql
@@ -0,0 +1,13 @@
+drop view if exists view_person;
+create view view_person as
+select prsn.prsn_id,
+    prsn.prsn_lname
+        ||case when prsn.prsn_lnamesuffix is null then '' else ', '||prsn.prsn_lnamesuffix end
+        ||', '||prsn.prsn_fname
+        ||case when prsn.prsn_fnamesuffix is null then '' else ' '||prsn.prsn_fnamesuffix end
+        ||case when prsn.prsn_mname is null then '' else ' '||prsn.prsn_minitial end
+    member_fullname,
+    prsn_gender,
+    prsn_birthdate,
+    prsn_civilstatus
+from person prsn;
