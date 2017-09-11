33e16bcbfe40125ed853e811151c426faca98898
Junjie <junjie@gbox.adnu.edu.ph>
2017-09-12 01:57:35 +0800

    loaded all javascripts similar to senpai.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,57 @@
 //
 
 //= require jquery-3.2.1
-//= require bootstrap/js/bootstrap.min
 //= require rails-ujs
 //= require turbolinks
 
+//= require jquery-ui.min
+//= require jquery.datatables.min
+//= require jquery.datatables2-custom.min
+//= require jquery.ddslick.min
+//= require jquery.sparkline.min
+
+//= require inputmask.min
+//= require ladda.min
+//= require parsley.min
+//= require selectize.min
+//= require spin.min
+
+//= require bootstrap/js/bootstrap.min
+//= require core/js/core
+//= require modernizr/js/modernizr.min
+
+//= require select2.full.min
+
 //= require script-custom
 //= require_tree .
+
+// ----- jquery
+// ----- jquery_ujs
+// ----- turbolinks
+// --------------- access
+// --------------- admission
+// app.min
+// --------------- applicants
+// --------------- department
+// --------------- home
+// --------------- index
+// ----- inputmask.min
+// ----- jquery-ui.min
+// ----- jquery.datatables.min
+// ----- jquery.ddslick.min
+// ----- jquery.sparkline.min
+// ----- ladda.min
+// ----- parsley.min
+// ---------------registrar
+// ---------------report
+// script-custom
+// ----- selectize.min
+// ---------------sessions
+// ----- spin.min
+// ---------------subjects
+// ----- bootstrap/js/bootstrap.min
+// ----- core/js/core
+// ----- jquery.datatables2-custom.min
+// ----- modernizr/js/modernizr.min
+// ----- select2.full.min
+// application
