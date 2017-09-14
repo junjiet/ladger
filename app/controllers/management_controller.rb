2b80bc3c1ec2f3421895b0c1ece5f67746b8fbac
Junjie <junjie@gbox.adnu.edu.ph>
2017-09-14 16:17:38 +0800

    added management/getMemberData.

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -1,5 +1,18 @@
 class ManagementController < ApplicationController
-  def members
-  	@members = View_member.order('prsn_fullname');
-  end
+	def members
+		@members = View_member.order('prsn_fullname');
+	end
+
+	# -----------------------------------------------------------------
+	private
+	def getMemberData(personID,dataField)
+		memberInfo = View_member.where('prsn_id=?',personID);
+
+		data = nil;
+		if (memberInfo.length>0)
+			data = memberInfo[0][dataField];
+		end
+
+		return data;
+	end
 end
