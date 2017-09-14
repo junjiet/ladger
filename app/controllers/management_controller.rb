db3d301a2e6612710832a07663634c7fdde0c344
Junjie <junjie@gbox.adnu.edu.ph>
2017-09-14 16:21:18 +0800

    added management/refreshData.

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -3,6 +3,29 @@ class ManagementController < ApplicationController
 		@members = View_member.order('prsn_fullname');
 	end
 
+	def refreshData
+		personID = params[:prsn_id];
+		dataGroup = params[:data_group];
+		dataField = params[:data_field];
+		@elementID = params[:element_id];
+		@elementType = params[:element_type];
+
+		if (dataGroup=='member')
+			@data = getMemberData(personID,dataField);
+			if (@data==nil)
+				@dataType = 'string';
+			else
+				if @data.is_a?(Module.const_get('Numeric'))
+					@dataType='numeric';
+				elsif @data.class.to_s=='ActiveSupport::TimeWithZone'
+					@dataType='datetime';
+				else
+					@dataType='string';
+				end
+			end
+		end
+	end
+
 	# -----------------------------------------------------------------
 	private
 	def getMemberData(personID,dataField)
