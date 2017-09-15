8c8bb9434f81ca5ef155c17ac86c7df4f091c374
Junjie <junjie@gbox.adnu.edu.ph>
2017-09-15 13:10:24 +0800

    management/members: implemented refreshData on #txtLname.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
 	root 'home#index';
 
+	get 'management/getData';
 	post 'management/setData';
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
